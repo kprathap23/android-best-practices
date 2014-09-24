098c9b8958460500dadf427d1492210cb906f70d
Nahuel Barrios <barrios.nahuel@gmail.com>
2014-09-24 18:59:26 -0300

    Refactor of ItemRepositoryImpl.java to encapsulate behavior; Encode user input when searching to be able to search "ipod nano";

--- app/src/main/java/com/nbempire/android/sample/repository/impl/ItemRepositoryImpl.java
+++ app/src/main/java/com/nbempire/android/sample/repository/impl/ItemRepositoryImpl.java
@@ -11,11 +11,12 @@ import org.apache.http.client.methods.HttpGet;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.json.JSONTokener;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -46,35 +47,34 @@ public class ItemRepositoryImpl implements ItemRepository {
         List<Item> items = new ArrayList<Item>();
 
         AndroidHttpClient androidHttpClient = AndroidHttpClient.newInstance("prueba");
-        HttpGet get = new HttpGet("https://api.mercadolibre.com/sites/MLA/search?q=" + titulo + "&limit=30");
+        HttpGet get = null;
         try {
+            get = new HttpGet("https://api.mercadolibre.com/sites/MLA/search?q=" + URLEncoder.encode(titulo, "UTF-8") + "&limit=30");
+
             HttpResponse response = androidHttpClient.execute(get);
             BufferedReader reader = new BufferedReader(new InputStreamReader(response.getEntity().getContent(), "UTF-8"));
-
-            StringBuilder builder = new StringBuilder();
-            for (String line = null; (line = reader.readLine()) != null; ) {
-                builder.append(line).append("\n");
-            }
-            JSONTokener tokener = new JSONTokener(builder.toString());
+            JSONObject object = new JSONObject(reader.readLine());
 
             androidHttpClient.close();
 
-            JSONObject object = new JSONObject(tokener);
-            Log.d(TAG, "resultado: " + object.toString());
-            String query = object.getString("query");
-
-            Log.d(TAG, "query vale: " + query);
-            Log.d(TAG, "site_id vale: " + object.getString("site_id"));
-
-            JSONArray results = object.getJSONArray(Keys.Search.RESULTS);
-            for (int i = 0; i < results.length(); i++) {
-                JSONObject eachObject = results.getJSONObject(i);
-                items.add(new Item(eachObject.getString(Keys.Item.TITLE)));
-            }
+            items = parseItemsJson(object.getJSONArray(Keys.Search.RESULTS));
+        } catch (UnsupportedEncodingException e) {
+            Log.e(TAG, "Error encoding user input for make an HTTP request: " + e.getMessage());
         } catch (IOException e) {
             Log.e(TAG, "Error executing GET: " + e.getMessage());
         } catch (JSONException e) {
             Log.e(TAG, "Error parsing response: " + e.getMessage());
+        }
+
+        return items;
+    }
+
+    private List<Item> parseItemsJson(JSONArray results) throws JSONException {
+        List<Item> items = new ArrayList<Item>();
+
+        for (int i = 0; i < results.length(); i++) {
+            JSONObject eachObject = results.getJSONObject(i);
+            items.add(new Item(eachObject.getString(Keys.Item.TITLE)));
         }
 
         return items;
