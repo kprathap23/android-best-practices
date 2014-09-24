bb7541420472d0b2397929a7fbdb6a67a9669999
Nahuel Barrios <barrios.nahuel@gmail.com>
2014-09-24 15:20:22 -0300

    Fix compilation problem.

--- app/src/main/java/com/nbempire/android/sample/SearchActivity.java
+++ app/src/main/java/com/nbempire/android/sample/SearchActivity.java
@@ -28,6 +28,6 @@ public class SearchActivity extends Activity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        reCreateturn id == R.id.action_settings || super.onOptionsItemSelected(item);
+        return id == R.id.action_settings || super.onOptionsItemSelected(item);
     }
 }
