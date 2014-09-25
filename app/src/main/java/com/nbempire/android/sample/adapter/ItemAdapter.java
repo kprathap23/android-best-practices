e82bd032af6fbabdd319559030d05d24d1bc26b7
Nahuel Barrios <barrios.nahuel@gmail.com>
2014-09-25 11:05:48 -0300

    Display subtitle and available quantity on landscape.

--- app/src/main/java/com/nbempire/android/sample/adapter/ItemAdapter.java
+++ app/src/main/java/com/nbempire/android/sample/adapter/ItemAdapter.java
@@ -7,6 +7,7 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
+import com.nbempire.android.sample.R;
 import com.nbempire.android.sample.domain.Item;
 
 /**
@@ -17,7 +18,7 @@ public class ItemAdapter extends ArrayAdapter<Item> {
     private final LayoutInflater layoutInflater;
 
     public ItemAdapter(Context context, Item[] items) {
-        super(context, android.R.layout.simple_list_item_1, items);
+        super(context, R.layout.item_in_list, items);
         layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
     }
 
@@ -29,13 +30,25 @@ public class ItemAdapter extends ArrayAdapter<Item> {
         View view;
 
         if (convertView == null) {
-            view = layoutInflater.inflate(android.R.layout.simple_list_item_1, parent, false);
+            view = layoutInflater.inflate(R.layout.item_in_list, parent, false);
         } else {
             view = convertView;
         }
 
-        Item anItem = getItem(position);
-        ((TextView) view.findViewById(android.R.id.text1)).setText(anItem.getTitulo());
+        Item cadaItem = getItem(position);
+
+        TextView titulo = (TextView) view.findViewById(R.id.item_title);
+        titulo.setText(cadaItem.getTitulo());
+
+        TextView subtitulo = (TextView) view.findViewById(R.id.item_subtitle);
+        if (subtitulo != null) {
+            subtitulo.setText(cadaItem.getSubtitulo());
+        }
+
+        TextView cantidadDisponible = (TextView) view.findViewById(R.id.item_quantity);
+        if (cantidadDisponible != null) {
+            cantidadDisponible.setText(cadaItem.getCantidadDisponible());
+        }
 
         return view;
     }
