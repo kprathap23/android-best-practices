594a8d597ea917db72cbdc080a17ef8bbf316685
Nahuel Barrios <barrios.nahuel@gmail.com>
2014-10-01 17:36:23 -0300

    Remove TODO.

--- app/src/main/java/com/nbempire/android/sample/component/service/ItemTrackerService.java
+++ app/src/main/java/com/nbempire/android/sample/component/service/ItemTrackerService.java
@@ -24,8 +24,6 @@ import java.util.List;
 /**
  * An {@link IntentService} subclass for handling asynchronous task requests in a service on a
  * separate handler thread.
- * <p/>
- * TODO: Customize class - update intent actions, extra parameters and static helper methods.
  */
 public class ItemTrackerService extends IntentService {
 
